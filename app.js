32030ba776a9000b5d353c0ad00610d8d1de87c9
sebasnain <sebasnain.dev@gmail.com>
2022-10-12 23:33:51 -0300

    realize unos cambios en las bandas y agregue un archivo js

---
+++ app.js
@@ -0,0 +1,24 @@
+
+
+let negro = "0";
+let marron = "1";
+let rojo = "2";
+let naranja="3";
+let amarrillo = "4";
+let verde = "5";
+let azul = "6";
+let violeta = "7";
+let gris = "8";
+let blanco = "9";
+
+
+
+function valorPorCodigo(banda1 ,banda2 ,banda3){
+    let resultado1 = banda1 + banda2 ;
+    let resultado2 = Number(resultado1)
+    let resultado3 = resultado2 * banda3;
+    console.log(resultado3)
+}
+
+let prueba = valorPorCodigo(rojo,rojo,10);
+
